a39d511df00fbeaffdf584ec1582d3b86c5e4d77
gpurkiss <purkissgrace@gmail.com>
2025-07-05 16:38:17 -0600

    finished prep4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+     List<int> numbers = new List<int>();
+        
+        // Please note we could use a do-while loop here instead
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a number from your list (0 to quit): ");
+            
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+            
+            // Only add the number to the list if it is not 0
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        // Part 1, the sum
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum of all numbers is: {sum}");
+
+        // part 2, the average
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average number is: {average}");
+
+        // part 3, the highest number
+        
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The highest number is: {max}");
     }
 }
