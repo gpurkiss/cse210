1b9ccb14daaf1143e1c413ebc926a9654b195964
gpurkiss <purkissgrace@gmail.com>
2025-07-20 10:59:57 -0600

    finished

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace ClassLibrary1;
+using System;
+using System.Threading.Tasks;
+public class Class1
+{
+    static async Task Main(string[] args)
+        {
+        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        await (1000);
+        Console.Clear();
+        await (5000);
+        Console.WriteLine("Goodbye World!");
+        }
+}
---
+++ ClassLibrary1/test.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Threading.Tasks;
+class Test{
+    static async Task Main(string[] args)
+        {
+Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        await (1000);
+        Console.Clear();
+        await (5000);
+        Console.WriteLine("Goodbye World!");
+        }
+}
